00354328870bdc32c768aa152f49706989d322b7
Henrik Söderström <henrik_s5@hotmail.com>
2018-04-23 23:03:46 +0200

    .

---
+++ Convert number to reversed array of digits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Convert_number_to_reversed_array_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        public static long[] Digitize(long n)
+        {
+
+            return new long[3] { 1, 2, 3 };
+        }
+    }
+}
