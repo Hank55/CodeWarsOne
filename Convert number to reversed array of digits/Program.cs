98a590f6ccedcfed105cb0bd90481d9ab70339ca
Henrik Söderström <henrik_s5@hotmail.com>
2018-04-25 22:28:18 +0200

    .

--- Convert number to reversed array of digits/Program.cs
+++ Convert number to reversed array of digits/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Convert_number_to_reversed_array_of_digits
 {
@@ -6,14 +7,31 @@ namespace Convert_number_to_reversed_array_of_digits
     {
         static void Main(string[] args)
         {
+            long number = 348597;
+
+            var longNumber = Digitize(number);
+
+            foreach (var item in longNumber)
+            {
+                Console.WriteLine(item);
+            }
 
         }
 
 
         public static long[] Digitize(long n)
         {
+            var stringArray = n.ToString();
 
-            return new long[3] { 1, 2, 3 };
+            var charArray = stringArray.ToCharArray();
+
+
+            long[] result = charArray.Select(i => long.Parse(i.ToString()))
+                .Reverse()
+                .ToArray();
+
+            return result;
         }
     }
 }
+  
