b101017d421b2efb0548d770dcdb13af5b0c6fcf
Henrik <henrik_s5@hotmail.comcleargit config --global user.email henrik_s5@hotmail.com>
2018-05-02 09:50:05 +0200

    -

---
+++ Make a function that does arithmetic!/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Make_a_function_that_does_arithmetic_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Arithmetic(5, 2, "add")); 
+            Console.WriteLine(Arithmetic(5, 2, "subtract"));
+            Console.WriteLine(Arithmetic(5, 2, "multiply"));
+            Console.WriteLine(Arithmetic(5, 2, "divide"));
+        }
+
+        public static double Arithmetic(double a, double b, string op)
+        {
+            if (op == "add")
+            {
+                return a + b;
+            }
+            else if (op == "subtract")
+            {
+                return a - b;
+            }
+            else if (op == "multiply")
+            {
+                return a * b;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Sorted_yes_no_how/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Sorted_yes_no_how
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSortedAndHow(new[] { 1, 2 ,3}));
+            Console.WriteLine(IsSortedAndHow(new[] { 15, 7, 3, -8 }));
+            Console.WriteLine(IsSortedAndHow(new[] { 4, 2, 30 }));
+
+        }
+
+
+        public static string IsSortedAndHow(int[] array)
+        {
+            var orderedByAsc = array.OrderBy(d => d);
+            var orderedByDesc = array.OrderByDescending(d => d);
+
+            if (array.SequenceEqual(orderedByAsc))
+            {
+                return "yes, ascending";
+            }
+            else if (array.SequenceEqual(orderedByDesc))
+            {
+                return "yes, descending";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+    }
+}
