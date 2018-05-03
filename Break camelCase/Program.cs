3a56309719ceeeaf9e8a5d1410b8fa9d03655140
Henrik Söderström <henrik_s5@hotmail.com>
2018-05-03 22:19:58 +0200

    .

---
+++ Break camelCase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace Break_camelCase
+{    //Complete the solution so that the function will break up camel casing, using a space between words.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BreakCamelCase("camelCasing"));
+        }
+
+        public static string BreakCamelCase(string str)
+        {
+           return Regex.Replace(str, "([A-Z])", " $1");
+        }
+    }
+}
