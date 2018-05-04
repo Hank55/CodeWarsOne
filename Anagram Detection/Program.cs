8b4537b739f8344a1f644312a55e11bbb81e1971
Henrik Söderström <henrik_s5@hotmail.com>
2018-05-05 00:25:09 +0200

    .

---
+++ Anagram Detection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Anagram_Detection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("foefet", "toffee"));
+            Console.WriteLine(IsAnagram("apple", "pale"));
+            Console.WriteLine(IsAnagram("Buckethead", "DeathCubeK"));
+        }
+
+        public static bool IsAnagram(string test, string original)
+        {
+            string aa = string.Concat(test.ToLower().OrderBy(c => c)).ToLower();
+            string bb = string.Concat(original.ToLower().OrderBy(d => d.ToString())).ToLower();
+
+            if (aa == bb)
+                return true;
+            else
+                return false;
+        }
+    }
+}
