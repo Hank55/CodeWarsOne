6bdd9f52100b98b44384b92dcba796d7da5687f1
Henrik Söderström <henrik_s5@hotmail.com>
2018-05-07 22:38:05 +0200

    .

---
+++ Sort array by string length/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_array_by_string_length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfWords = new string[] { "Telescopes", "Glasses", "Eyes", "Monocles" };
+            var sorted = SortByLength(listOfWords);
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static string[] SortByLength(string[] array)
+        {
+            var sortedValues = array.OrderBy(x => x.Length).ToArray();
+            return sortedValues;
+        }
+    }
+}
