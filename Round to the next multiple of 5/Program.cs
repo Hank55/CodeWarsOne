c0a28366d04cb9f20ac4551360c33adc39ea5a91
Henrik <henrik_s5@hotmail.comcleargit config --global user.email henrik_s5@hotmail.com>
2018-05-16 22:23:50 +0200

    .

---
+++ Round to the next multiple of 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Round_to_the_next_multiple_of_5
+{
+    class Program
+    {
+        //Given an integer as input, can you round it to the next (meaning, "higher") 5?
+        static void Main(string[] args)
+        {
+
+            //UseParams("HellO", 1, 'a');
+
+
+            Console.WriteLine(RoundToNext5(2));
+            Console.WriteLine(RoundToNext5(8));
+            Console.WriteLine(RoundToNext5(-18));
+        }
+
+        //public static void UseParams(params object[] list)
+        //{
+
+        //    for (int i = 0; i < list.Length; i++)
+        //    {
+        //       Console.Write(list[i] + " ");
+        //    }
+        //    Console.WriteLine();
+
+        //}
+
+
+        public static int RoundToNext5(int n)
+        {
+            return (n % 5 == 0) ? n : RoundToNext5(n + 1);
+        }
+    }
+}
---
+++ Vampire Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vampire_Numbers
+{
+    class Program
+    {
+        //Our loose definition of Vampire Numbers can be described as follows
+        static void Main(string[] args)
+        {
+            Console.WriteLine(vampire_test(21, 6));
+            Console.WriteLine(vampire_test(22, 6));
+        }
+
+        public static bool vampire_test(long x, long y)
+        {
+            return (x * y).ToString().OrderBy(c => c).SequenceEqual((x.ToString()+ y).OrderBy(c => c));
+        }
+    }
+}
