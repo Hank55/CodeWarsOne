fab2cfab0772ad670ae69c89a8ce0058484865a4
Henrik Söderström <henrik_s5@hotmail.com>
2018-06-18 12:14:31 +0200

    .

---
+++ Find the odd int/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Find_the_odd_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(greet("Niks"));
+            Console.WriteLine(greet(""));
+            Console.WriteLine(greet(null));
+        }
+
+        public static string greet(string name)
+        {
+            if (name == null)
+            {
+                return null;
+            }
+            else if (name == string.Empty)
+            {
+                return null;
+            }
+            else
+            {
+                return "hello " + name + "!";
+            }
+        }
+    }
+}
