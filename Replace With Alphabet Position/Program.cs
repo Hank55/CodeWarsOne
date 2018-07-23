f8411d8be084fe94f1f36737ad225d94c4d81e04
Henrik Söderström <henrik_s5@hotmail.com>
2018-07-23 22:09:24 +0200

    -

---
+++ Replace With Alphabet Position/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Replace_With_Alphabet_Position
+{
+    /*Welcome.
+    In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+    If anything in the text isn't a letter, ignore it and don't return it.
+    a being 1, b being 2, etc.
+    As an example:*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AlphabetPosition("The sunset sets at twelve o' clock."));
+            Console.WriteLine(AlphabetPosition("The narwhal bacons at midnight."));
+        }
+
+
+        public static string AlphabetPosition(string text)
+        {
+            var newText = Regex.Replace(text, "[^a-zA-Z0-9]", "");
+
+            var charArray = newText.ToCharArray();
+
+            List<int> vs = new List<int>();
+
+            foreach (var item in charArray)
+            {
+                int index = char.ToUpper(item) - 64;//index == 1
+                vs.Add(index);
+            }
+
+            string textString = string.Empty;
+            foreach (var item in vs)
+            {
+                textString = textString + item.ToString()+ " ";
+            }
+
+            var result = textString.TrimEnd();
+
+            return result;
+        }
+    }
+}
---
+++ Summing a number's digits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Summing_a_number_s_digits
+{
+    //Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigits(-12));
+        }
+
+
+        public static int SumDigits(int number)
+        {
+            var numbers = Math.Abs(number);
+
+            var sum = 0;
+
+            int[] outarry = Array.ConvertAll(numbers.ToString().ToArray(), x => (int)x - 48);
+
+            foreach (var item in outarry)
+            {
+                sum = sum + item;
+            }
+
+            return sum;
+        }
+    }
+}
