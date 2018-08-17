cc00243490a53586ab68ac1058d62fbdefe6ec4c
Henrik Söderström <henrik_s5@hotmail.com>
2018-08-17 09:30:58 +0200

    .

---
+++ Friend or Foe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Friend_or_Foe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+
+            var result = FriendOrFoe(names);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+                
+        }
+
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            return names.Where(n => n.Length == 4);
+        }
+    }
+}
